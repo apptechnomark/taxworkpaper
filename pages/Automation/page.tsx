6ccb824f4d685d11c69d0ef0bc2242251b5a10d7
PACIFICABS\bhargav.lakhani <bhargav.lakhani@technomark.io>
2023-09-19 18:46:57 +0530

    loader

--- pages/Automation/page.tsx
+++ pages/Automation/page.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import Spinner from "tsconfig.json/components/Spinner`";
 import { ToastContainer, ToastOptions, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Image from "next/image";
+import loader from "../../public/loder.gif";
 
 const toastOptions: ToastOptions = {
   position: "top-right",
@@ -50,11 +52,6 @@ const Automation = () => {
     setDownloadBtn(false);
   };
 
-  // const handelDownload = async () => {
-  //   // document.getElementById("downloadClick")?.click();
-  //   toast.success("File automatic start downloading...!!!", toastOptions)
-  // };
-
   const handleUpload = async (e: any) => {
     e.preventDefault();
     setDisabled(true);
@@ -86,102 +83,110 @@ const Automation = () => {
 
   return (
     <>
-      <section className="automationSection px-5 py-9">
-        <div className="container mx-auto px-20">
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <ToastContainer
-              position="top-center"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-            />
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr className="text-center">
-                  <th scope="col" className="px-6 py-3 text-start">
-                    Indexing & Bookmarking
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-center">
-                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-start">
-                    <input
-                      className="w-full text-sm text-gray-900 border border-gray-300 cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                      aria-describedby="file_input_help"
-                      id="file_input"
-                      type="file"
-                      onChange={handleChange}
-                    />
-                    {fileError && (
-                      <p
-                        className="mt-1 text-sm text-red-500 dark:text-red-300"
-                        id="file_input_help"
-                      >
-                        {fileErrMsg}
-                      </p>
-                    )}
-                    {!fileError && (
-                      <p
-                        className="mt-1 text-sm text-gray-500 dark:text-gray-300"
-                        id="file_input_help"
-                      >
-                        Upload your ZIP file here.
-                      </p>
-                    )}
-                  </td>
-                  <td className="flex px-6 py-4 gap-[15px] justify-center">
-                    <button
-                      className={`flex gap-[15px] bg-[#1492c8] text-white text-sm font-semibold px-4 py-2 rounded-md ${
-                        disabled || fileError || !uploadBtn
-                          ? "cursor-not-allowed opacity-50"
-                          : ""
-                      }`}
-                      onClick={
-                        disabled || !uploadBtn || fileError
-                          ? undefined
-                          : handleUpload
-                      }
-                    >
-                      Upload
-                      {disabled ? (
-                        <>
-                          <Spinner />
-                        </>
-                      ) : (
-                        ""
+      {disabled ? (
+        <>
+          <div className="flex justify-center items-center min-h-[calc(100vh-70px)] bg-[#fcfcff]">
+            <Image src={loader} alt="Loader" />
+          </div>
+        </>
+      ) : (
+        <section className="automationSection px-5 py-9">
+          <div className="container mx-auto px-20">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+              />
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr className="text-center">
+                    <th scope="col" className="px-6 py-3 text-start">
+                      Indexing & Bookmarking
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-center">
+                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-start">
+                      <input
+                        className="w-full text-sm text-gray-900 border border-gray-300 cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                        aria-describedby="file_input_help"
+                        id="file_input"
+                        type="file"
+                        onChange={handleChange}
+                      />
+                      {fileError && (
+                        <p
+                          className="mt-1 text-sm text-red-500 dark:text-red-300"
+                          id="file_input_help"
+                        >
+                          {fileErrMsg}
+                        </p>
                       )}
-                    </button>
-                    <a
-                      id="downloadClick"
-                      href={downloadBtn ? downloadUrl : "javascript:void(0);"}
-                      className={`flex gap-[15px] bg-[#259916] text-white text-sm font-semibold px-4 py-2 rounded-md ${
-                        downloadBtn ? "" : "cursor-not-allowed opacity-50"
-                      }`}
-                    >
-                      {/* onClick={
+                      {!fileError && (
+                        <p
+                          className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+                          id="file_input_help"
+                        >
+                          Upload your ZIP file here.
+                        </p>
+                      )}
+                    </td>
+                    <td className="flex px-6 py-4 gap-[15px] justify-center">
+                      <button
+                        className={`flex gap-[15px] bg-[#1492c8] text-white text-sm font-semibold px-4 py-2 rounded-md ${
+                          disabled || fileError || !uploadBtn
+                            ? "cursor-not-allowed opacity-50"
+                            : ""
+                        }`}
+                        onClick={
+                          disabled || !uploadBtn || fileError
+                            ? undefined
+                            : handleUpload
+                        }
+                      >
+                        Upload
+                        {disabled ? (
+                          <>
+                            <Spinner />
+                          </>
+                        ) : (
+                          ""
+                        )}
+                      </button>
+                      <a
+                        id="downloadClick"
+                        href={downloadBtn ? downloadUrl : "javascript:void(0);"}
+                        className={`flex gap-[15px] bg-[#259916] text-white text-sm font-semibold px-4 py-2 rounded-md ${
+                          downloadBtn ? "" : "cursor-not-allowed opacity-50"
+                        }`}
+                      >
+                        {/* onClick={
                       downloadBtn
                         ? () => handelDownload(images)
                         : undefined
                     } */}
-                      Download
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+                        Download
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   );
 };
