ae1838db2ab63f48d03c176a6b098e63d79aab5d
PACIFICABS\bhargav.lakhani <bhargav.lakhani@technomark.io>
2023-09-19 18:12:01 +0530

    changes given by maulik sir

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="header bg-[#1492c8] p-5">
-      <Image src="/logo.svg" width={150} height={150} alt="Logo" />
+    <div className="header bg-[#1492c8] py-2.5">
+      <div className="container mx-auto px-20">
+        <Image src="/logo.svg" width={150} height={150} alt="Logo" />
+      </div>
     </div>
   );
 }
